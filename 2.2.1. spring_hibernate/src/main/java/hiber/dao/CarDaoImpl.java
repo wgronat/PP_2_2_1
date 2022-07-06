23e756a063ea3e3c9bcab3e4b82558556708ee48
wgronat <ildebote@bk.ru>
2022-07-06 10:02:55 +0500

    добавил слой dao и service для car

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImpl.java
@@ -24,6 +24,7 @@ public class CarDaoImpl implements CarDao{
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Car> listCars() {
         TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car");
         return query.getResultList();
