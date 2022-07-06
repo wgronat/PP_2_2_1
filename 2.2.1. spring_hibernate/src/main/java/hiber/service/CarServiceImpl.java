5a0c66afc9007520cd8ad615e7410bd71c693082
wgronat <ildebote@bk.ru>
2022-07-06 10:02:15 +0500

    добавил слой dao и service для car

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package hiber.service;
+
+import hiber.dao.CarDao;
+import hiber.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService{
+
+    private final CarDao carDao;
+
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public void add(Car car) {
+        carDao.add(car);
+    }
+
+    @Override
+    public List<Car> listCars() {
+        return carDao.listCars();
+    }
+}
